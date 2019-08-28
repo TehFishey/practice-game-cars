9406b0937aa5da035c8ee075c03d7a95c67e0081
Michael D'Attilo <anuncertaintruth@gmail.com>
2019-08-27 20:15:17 -0400

    Added Basic Draw Engine
    Added classes and inits for display, physics, and a basic shape factory.
    
    Sources: https://www.youtube.com/watch?v=L0n7vUeSS90 https://www.youtube.com/watch?v=YX1HwC3YTGA

--- core/src/game/fish/cars/CarsGame.java
+++ core/src/game/fish/cars/CarsGame.java
@@ -1,33 +1,26 @@
 package game.fish.cars;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class CarsGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+import game.fish.cars.screens.PlayScreen;
+
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class CarsGame extends Game {
+	public void create() {
+		setScreen(new PlayScreen());
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		super.dispose();
 	}
 }
---
+++ core/src/game/fish/cars/screens/PlayScreen.java
@@ -0,0 +1,105 @@
+package game.fish.cars.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import game.fish.cars.tools.ShapeFactory;
+
+import static game.fish.cars.Constants.GRAVITY;
+import static game.fish.cars.Constants.DEFAULT_ZOOM;
+import static game.fish.cars.Constants.PPM;
+
+public class PlayScreen implements Screen {
+
+	private final SpriteBatch mBatch;
+	private final World mWorld;
+	private final Box2DDebugRenderer mB2debug;
+	private final OrthographicCamera mCamera;
+	private final Viewport mViewport;
+	private final Body mPlayer;
+	
+	public PlayScreen() {
+		mBatch = new SpriteBatch();
+		mWorld = new World(GRAVITY, true);
+		mB2debug = new Box2DDebugRenderer();
+		mCamera = new OrthographicCamera();
+		mViewport = new FitViewport(640 / PPM, 480 / PPM, mCamera);
+		mPlayer = ShapeFactory.createRectangle(new Vector2(0,0), new Vector2(64,128), BodyDef.BodyType.DynamicBody, mWorld, 0.4f);
+		
+		mCamera.zoom = DEFAULT_ZOOM;
+		
+		
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		// screen loop
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		update(delta);
+		draw();
+	}
+		
+	private void update(final float delta) {
+		mCamera.position.set(mPlayer.getPosition(), 0);
+		mCamera.update();
+		
+		mWorld.step(delta, 6, 2);
+	}
+		
+	private void draw() {
+		mBatch.setProjectionMatrix(mCamera.combined);
+		mB2debug.render(mWorld, mCamera.combined);
+	}
+		
+	
+
+	@Override
+	public void resize(int width, int height) {
+		mViewport.update(width, height);
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		mBatch.dispose();
+		mWorld.dispose();
+		mB2debug.dispose();
+	}
+
+}
